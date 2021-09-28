dffa8afe2df07047a204b3fcc06ab4a2226ebfc3
tsukasamuto55 <tsukasa.muto@live.com>
2021-09-28 00:39:14 -0600

    CSS for each page was updated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WDD-330
-Portfolio page for WDD 330
+
+Portfolio page for WDD 330 at Byu Idaho Fall 2021.
---
+++ js/typeEffect.js
@@ -0,0 +1,14 @@
+const title = document.querySelector(".title");
+const strings = "Hello, my name is Tsukasa Muto";
+const strings2 = "I'm a passionate Front End Developer";
+const splitStrings = strings.split("");
+const splitStrings2 = strings2.split("");
+let content = "";
+
+const typingAnimation = setInterval(() => {
+  title.innerText = content += splitStrings.shift();
+
+  if (splitStrings == "") {
+    clearInterval(typingAnimation);
+  }
+}, 175);
